f83395307d418dc404eae26d56619e1248c182e7
akal <muazuawe2223@gmail.com>
2026-07-20 17:35:48 +0100

    Move migrations and superuser creation to wsgi.py, turn off DEBUG

--- lgea_portal/settings.py
+++ lgea_portal/settings.py
@@ -11,7 +11,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 
 SECRET_KEY = config('SECRET_KEY', default='django-insecure-lgea-staff-school-keffi-nasarawa-2024-secret-key-change-in-production')
 
-DEBUG = True
+DEBUG = config('DEBUG', default=False, cast=bool)
 
 ALLOWED_HOSTS = ['*', '.vercel.app', '.now.sh']
 
