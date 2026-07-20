50eec4403b7d066b1f7e1088d3b44fd212a0b4e3
akal <muazuawe2223@gmail.com>
2026-07-20 17:30:08 +0100

    Update build script to allow admin password resets

--- build_files.sh
+++ build_files.sh
@@ -15,9 +15,15 @@ from django.contrib.auth.models import User
 username = os.environ.get('DJANGO_SUPERUSER_USERNAME', 'admin')
 email = os.environ.get('DJANGO_SUPERUSER_EMAIL', 'admin@lgeaschool.com')
 password = os.environ.get('DJANGO_SUPERUSER_PASSWORD', '')
-if password and not User.objects.filter(username=username).exists():
-    User.objects.create_superuser(username, email, password)
-    print(f'Superuser {username} created successfully')
+if password:
+    if not User.objects.filter(username=username).exists():
+        User.objects.create_superuser(username, email, password)
+        print(f'Superuser {username} created successfully')
+    else:
+        u = User.objects.get(username=username)
+        u.set_password(password)
+        u.save()
+        print(f'Superuser {username} password updated successfully')
 else:
-    print(f'Superuser {username} already exists or no password set')
+    print(f'No password set in DJANGO_SUPERUSER_PASSWORD')
 "
